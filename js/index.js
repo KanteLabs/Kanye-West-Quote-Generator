a2a8e9335c1883bc46a5b14321ec868dc4788126
Amidou Kante <kamdiou@gmail.com>
2017-07-20 13:58:25 -0400

    Fixed quotes issue

--- js/index.js
+++ js/index.js
@@ -30,11 +30,13 @@ quotes = [
 
 function randomQuote() {
   var name = " ~Kanye West";
-  $.get("https://yepi.io/api/quote", function(data) {
-    $("#currentquote").html(data + name);
-    $("#tweet").attr("href", "http://twitter.com/home/?status=" + data +
-      ' (' + name + ')');
-  });
+  var max = Math.floor(Math.random()*28);
+  var randomQuote = quotes[max];
+
+  console.log(randomQuote);
+
+    $("#currentquote").html(randomQuote + name);
+    $("#tweet").attr("href", "http://twitter.com/home/?status=" + randomQuote +' (' + name + ')');
 }
 $(function() {
   randomQuote();
